70e21c61b69f2f4ddbb9c0c3e0851f7e995b9428
GalinaMuratova <ufkz200147@gmail.com>
2023-06-08 01:26:12 +0600

    Удалены лишние файлы. Добавленны компоненты UserForm и User. Подключен bootstrap.css

---
+++ src/components/User/User.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const User = () => {
+    return (
+        <div>
+            <h4>User 1</h4>
+            <h4>User 1</h4>
+            <h4>User 1</h4>
+        </div>
+    );
+};
+
+export default User;
